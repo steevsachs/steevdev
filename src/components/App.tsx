3584a67053d89a384e3272378528398d69c85fea
Stephen Sachs <steev.sachs@gmail.com>
2019-04-19 20:18:03 -0400

    Add fonts to now.json

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { css } from 'emotion'
 import { textStyles } from '../styles'
 import MenuContainer from './MenuContainer'
 import MenuContent from './MenuContent'
+import MenuItem from './MenuItem'
 import Portrait from './Portrait'
 import React from 'react'
-import MenuItem from './MenuItem'
 
 enum Sections {
   Bio = 'bio',
@@ -37,34 +38,36 @@ const styles = {
 }
 
 const App: React.FC = () => (
-  <div className={styles.container}>
-    <div className={styles.section}>
-      <MenuContainer>
-        <Portrait />
-        <MenuContent>
-          <div>STEEV SACHS</div>
-          <div>Software Engineer</div>
+  <Router>
+    <div className={styles.container}>
+      <div className={styles.section}>
+        <MenuContainer>
+          <Portrait />
+          <MenuContent>
+            <div>STEEV SACHS</div>
+            <div>Software Engineer</div>
+            <div>
+              <span className={textStyles.blueText}>XP: </span> 6 yrs
+            </div>
+          </MenuContent>
+        </MenuContainer>
+      </div>
+      <div className={css(styles.section, styles.body)}>
+        <MenuContainer className={styles.menu}>
+          <MenuItem selected>Bio</MenuItem>
+          <MenuItem>Skills</MenuItem>
+          <MenuItem>Links</MenuItem>
+          <MenuItem>Hire</MenuItem>
+        </MenuContainer>
+        <MenuContainer className={styles.main}>
           <div>
-            <span className={textStyles.blueText}>XP: </span> 6 yrs
+            Full stack engineer seeking opportunities to contribute to the development of efficient,
+            innovative, high-quality software and processes as part of a team of passionate
+            professionals who love their jobs and their products
           </div>
-        </MenuContent>
-      </MenuContainer>
+        </MenuContainer>
+      </div>
     </div>
-    <div className={css(styles.section, styles.body)}>
-      <MenuContainer className={styles.menu}>
-        <MenuItem selected>Bio</MenuItem>
-        <MenuItem>Skills</MenuItem>
-        <MenuItem>Links</MenuItem>
-        <MenuItem>Hire</MenuItem>
-      </MenuContainer>
-      <MenuContainer className={styles.main}>
-        <div>
-          Full stack engineer seeking opportunities to contribute to the development of efficient,
-          innovative, high-quality software and processes as part of a team of passionate
-          professionals who love their jobs and their products
-        </div>
-      </MenuContainer>
-    </div>
-  </div>
+  </Router>
 )
 export default App
